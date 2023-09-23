b14df6990f3ecd23c05e9157b8d41ee2f83e4b5d
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-09-23 14:02:02 -0400

    Java init Commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+// class with the same name of the file - use Pascal Case (i.e. BankAccount)
+public class Test {
+	// "public static void" is equivalent to "function" in Js
+	// 'main' is the name of the method public static void
+	public static void main(String[] args) {
+			System.out.println("Hello World!");
+	}
+}
+
+// NEXT 'javac Test.java' is typed in the terminal -> Test.class gets created
+// The above complies the code into bytecode into a *.class file--- the below evokes the file
+// NEXT 'java Test' into terminal
